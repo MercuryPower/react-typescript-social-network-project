b87c5b167cf16f3d42ae79b229cd7aace075f0d2
Nikita Holod <75911800+MercuryPower@users.noreply.github.com>
2023-07-25 19:17:42 +0400

    Added a modal window when clicking on the trash can when deleting a post.
    Modification of Modal Window + on press ESC closing functionality

--- src/UI/Modal/ModalWindow.tsx
+++ src/UI/Modal/ModalWindow.tsx
@@ -1,44 +1,114 @@
-import React, {useState} from 'react';
+import React, {useEffect, useMemo, useState} from 'react';
 import Button from "../Button/Button";
 import {Modal} from "react-bootstrap";
-import styled from "styled-components";
+import styled, {css, keyframes} from "styled-components";
+import Flex from "../../components/Flex";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faX} from "@fortawesome/free-solid-svg-icons/faX";
+import {faCheck} from "@fortawesome/free-solid-svg-icons";
+import {faTimes} from "@fortawesome/free-solid-svg-icons/faTimes";
 
 interface ModalProps {
-    show:boolean
+    show:boolean;
+    onClose:() => void;
+    onConfirm:() => void;
+    children: React.ReactNode;
+    confirmButtonText: string;
+    cancelButtonText: string;
 }
 
-const StyledModalWindow = styled.div<ModalProps>`
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+`;
+const fadeOut = keyframes`
+  from {
+    opacity: 1;
+  }
+  to {
+    opacity: 0;
+  }
+`;
+
+const StyledModalWindow = styled.div`
   position: fixed;
   top:0;
   bottom: 0;
   left: 0;
   right: 0;
-  display: ${(props) => (props.show ? 'block' : 'none')}
-`
-const ModalWindow = () => {
-    const [show, setShow] = useState(true);
+  display: ${(props:{show:boolean}) => (props.show ? 'flex' : 'none') };
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.5);
+  ${props => props.show && css`
+    opacity: 1;
+    animation: ${fadeIn} 0.3s ease;
+    `}
+  ${props => !props.show && css`
+    opacity:0;
+    animation:${fadeOut} 0.3s ease;
+    `}
+  `;
+const ModalContent = styled.div`
+  position: relative;
+  background-color: #fff;
+  border-radius: 8px;
+  padding: 20px;
+  max-width: 400px;
+  text-align: center;
+ 
+`;
+const CloseButton = styled.button`
+  position: inherit;
+  left:20px;
+  bottom: 15px;
+  background: transparent;
+  border: none;
+  cursor: pointer;
+`;
 
-    const handleClose = () => setShow(false);
-    const handleYes = () => setShow(true);
+const ModalWindow:React.FC<ModalProps> = ({show, onConfirm, onClose, children, confirmButtonText, cancelButtonText}) => {
+
+    // Закрытие окна через ESC
+    useMemo(() => {
+        const handleKeyPress = (event: KeyboardEvent) => {
+            if (event.key === 'Escape') {
+                onClose();
+            }
+        };
+        document.addEventListener('keydown', handleKeyPress)
+
+        return () => {
+            document.removeEventListener('keydown', handleKeyPress);
+            console.log('I did it!')
+        };
+    }, [onClose]);
+    // !Закрытие окна через ESC
 
     return (
         <>
             <StyledModalWindow show={show}>
-                <Modal.Header closeButton>
-                    <Modal.Title>Are you sure?</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>The data will be lost.</Modal.Body>
-                <Modal.Footer>
-                    <Button onClick={handleYes}>
-                        Yes, I'm sure!
-                    </Button>
-                    <Button onClick={handleClose}>
-                        Nope.
-                    </Button>
-                </Modal.Footer>
+                <ModalContent>
+                    {children}
+                    <CloseButton onClick={onClose}>
+                        <FontAwesomeIcon icon={faTimes} />
+                    </CloseButton>
+                    <Flex justifyContent={'center'} alignItems={'center'} margin={'15px'}>
+                        <Button color={'black'} radius={'25px'} margin={'0 10px 0 0'} onClick={onConfirm}>
+                            <FontAwesomeIcon icon={faCheck} /><b>{confirmButtonText}</b>
+                        </Button>
+                        <Button background={'black'} color={'white'} radius={'25px'} onClick={onClose}>
+                            <FontAwesomeIcon icon={faX} /><b>{cancelButtonText}</b>
+                        </Button>
+                    </Flex>
+                </ModalContent>
             </StyledModalWindow>
         </>
     );
 };
 
-export default Modal;
+export default ModalWindow;
